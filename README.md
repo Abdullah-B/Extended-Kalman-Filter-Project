28189879ba0e065c125fc53da0c2aa0bf9b567a9
Abdullah Ali <abdullah.bw.ali@gmail.com>
2018-04-30 12:08:55 +0300

    intial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Extended Kalman Filter Project Starter Code
+Self-Driving Car Engineer Nanodegree Program
+
+In this project we  utilized a kalman filter to estimate the state of a moving object of interest with noisy lidar and radar measurements.  
+
+
+## Basic Build Instructions
+
+1. Clone this repo.
+2. Make a build directory: `mkdir build && cd build`
+3. Compile: `cmake .. && make` 
+   * On windows, you may need to run: `cmake .. -G "Unix Makefiles" && make`
+4. Run it: `./ExtendedKF `
+
+
+## Changes and Results
+
+as per what was requested for this project the following files were modified to fullfil the requirments of the project
+
+   ### 1 : FusionEKF.cpp
+
+Finished all the initializations indicated in the comments
+
+   ### 2 : FusionEKF.h
+
+added   float noise_ax;
+        float noise_ay;
+
+   ### 3 : kalman_filter.cpp
+
+Coded the Predict(),Update(const VectorXd &z), and UpdateEKF(const VectorXd &z)
+
+   ### 4 : Kalman_filter.h
+
+added   Tools tools;
+and
+        Eigen::MatrixXd Hj_; 
+
+   ### 5 : tools.cpp
+
+added the code to calculate the RMSE inside 
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth)
+and the code to calculate the Jacobian inside 
+    MatrixXd Tools::CalculateJacobian(const VectorXd& x_state)
+    
+   ### The resutls were
+    
+   ![RESULTS](OutPut & Results/Result.PNG)
+   
+   and the Matrix outputs are documented inside OutPut & Results/ Consol_OUT_PUT.txt
+   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/kalman_filter.cpp
@@ -0,0 +1,92 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+// Please note that the Eigen library does not initialize 
+// VectorXd or MatrixXd objects with zeros upon creation.
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+	x_ =F_*x_;
+	MatrixXd Ft = F_.transpose();
+	P_= F_*P_*Ft+Q_;
+
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+	VectorXd y = z - H_ * x_;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * (P_ * Ht) + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K =  P_ * Ht * Si;
+	MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+
+	//new state
+	x_ = x_ + (K * y);
+	P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+	float px = x_(0);
+ 	float py = x_(1);
+ 	float vx = x_(2);
+ 	float vy = x_(3);
+
+ 	float rho = sqrt(px*px+py*py);
+	float theta = atan2(py,px);
+	float ro_dot;
+
+	if(px == 0 && py == 0)
+		{ro_dot = 0;}
+	else
+		{ro_dot = (px*vx+py*vy)/rho;}
+
+	
+	Hj_=tools.CalculateJacobian(x_);
+
+
+	VectorXd z_pred = VectorXd(3);
+	z_pred << rho,theta,ro_dot;
+
+	VectorXd y = z - z_pred;
+
+	double width = 2 * M_PI;   //
+  	double offsetValue = y(1) + M_PI;   // value relative to 0
+  	y(1) = (offsetValue - (floor(offsetValue / width) * width)) - M_PI;
+
+	MatrixXd Hjt = Hj_.transpose();
+	MatrixXd S = Hj_ * (P_ * Hjt) + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K =  P_ * Hjt * Si;
+	MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+
+	//new state
+	x_ = x_ + (K * y);
+	P_ = (I - K * Hj_) * P_;
+}
